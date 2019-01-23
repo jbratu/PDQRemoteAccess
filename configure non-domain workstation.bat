822cc929543cc62d7da17b690ec829f3eb609cac
Jared Bratu <jared@thecongruity.com>
2019-01-23 10:37:19 -0500

    Add comments and adjusted configure script to force add registry entry

--- configure non-domain workstation.bat
+++ configure non-domain workstation.bat
@@ -1,4 +1,5 @@
+REM Don't forget to run this as an administrator in an elevated command prompt and reboot.
 netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol=icmpv4:8,any dir=in action=allow
 netsh advfirewall firewall set rule group="File and Printer Sharing" new enable=Yes
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v LocalAccountTokenFilterPolicy /t REG_DWORD /d 1
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /f /v LocalAccountTokenFilterPolicy /t REG_DWORD /d 1
 shutdown /r
--- unconfigure non-domain workstation.bat
+++ unconfigure non-domain workstation.bat
@@ -1,3 +1,4 @@
+REM Don't forget to run this as an administrator in an elevated command prompt and reboot.
 netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol=icmpv4:8,any dir=in action=block
 netsh advfirewall firewall set rule group="File and Printer Sharing" new enable=No
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v LocalAccountTokenFilterPolicy /t REG_DWORD /d 0
